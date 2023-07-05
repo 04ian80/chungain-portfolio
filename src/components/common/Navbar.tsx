c20bbde56de4d6830e93f2ce47bfd7f1e0f27517
Ain Chung <artnouveau0804@gmail.com>
2023-07-05 11:14:11 +0900

    chore: fix typo

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -2,7 +2,7 @@ import Menu from './Menu';
 
 const navLinks = [
   { title: '소개', link: '/introduction' },
-  { title: '기술스택', link: '/skill' },
+  { title: '기술스택', link: '/skills' },
   { title: '프로젝트', link: '/project' },
   { title: '교육/활동', link: '/activity' },
 ];
