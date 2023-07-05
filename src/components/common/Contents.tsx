e7c4534c8a72a01474dd156754c36081e0fcbf2c
Ain Chung <artnouveau0804@gmail.com>
2023-07-05 09:48:26 +0900

    feat: implements Activity component

---
+++ src/components/common/Contents.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type ContentsType = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const Contents = ({ title, children }: ContentsType) => {
+  return (
+    <section className='contents'>
+      <h3 className='contents__heading'>{title}</h3>
+      {children}
+    </section>
+  );
+};
+
+export default Contents;
